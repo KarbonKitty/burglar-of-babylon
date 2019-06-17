5a3c28836569d4d38f3bc6a8b1fcea472ec88348
KarbonKitty <308lapua.magnum@gmail.com>
2019-06-17 20:21:17 +0200

    Part 1 - move @ around
    Includes formatting changes, basic main loop,
    and magic numbers removal

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
-import { Display } from 'rot-js';
+import { Display, KEYS } from 'rot-js';
 
-const gameDisplay = new Display({ width: 60, height: 60, forceSquareRatio: true, fontSize: 13 });
-const messageDisplay = new Display({ width: 80, height: 60, fontSize: 13}); 
+const displayConstants = {
+    mainAreaWidth: 60,
+    mainAreaHeight: 60,
+    textAreaWidth: 80,
+    textAreaHeight: 60,
+    fontSize: 13
+};
+
+const gameDisplay = new Display({
+    width: displayConstants.mainAreaWidth,
+    height: displayConstants.mainAreaHeight,
+    forceSquareRatio: true,
+    fontSize: displayConstants.fontSize,
+    fontFamily: "arial"
+});
+
+const messageDisplay = new Display({
+    width: displayConstants.textAreaWidth,
+    height: displayConstants.textAreaHeight,
+    fontSize: displayConstants.fontSize
+});
 
 const gameContainer = gameDisplay.getContainer()!;
 const messageContainer = messageDisplay.getContainer()!;
@@ -11,5 +30,34 @@ const rightDiv = document.getElementById("right")!;
 leftDiv.appendChild(gameContainer);
 rightDiv.appendChild(messageContainer);
 
-gameDisplay.draw(30, 30, '@', '#fff', '#000');
-messageDisplay.drawText(2, 2, 'Hello world!', 75);
+const playerData = {
+    x: displayConstants.mainAreaWidth / 2,
+    y: displayConstants.mainAreaHeight / 2
+};
+
+window.onload = () => {
+    // input handler
+    window.addEventListener("keydown", e => {
+        switch (e.keyCode) {
+            case KEYS.VK_UP:
+                playerData.y -= 1;
+                break;
+            case KEYS.VK_DOWN:
+                playerData.y += 1;
+                break;
+            case KEYS.VK_LEFT:
+                playerData.x -= 1;
+                break;
+            case KEYS.VK_RIGHT:
+                playerData.x += 1;
+                break;
+        }
+    });
+
+    // main loop
+    window.setInterval(() => {
+        gameDisplay.clear();
+        gameDisplay.draw(playerData.x, playerData.y, '@', '#fff', '#000');
+        messageDisplay.drawText(2, 2, 'Hello world!', 75);
+    }, 50);
+};
